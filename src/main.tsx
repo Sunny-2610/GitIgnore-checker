2c7729fff2f20382dcd0e64ce1b4c6ac27881c17
Sunny-2610 <sunnysinha9884@gmail.com>
2025-10-28 22:26:51 +0530

    bug fix 2

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 import { LanguageProvider } from './components/LanguageContext.tsx';
+import { TooltipProvider } from './components/ui/tooltip'; // Add this import
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
-      <App />
+      <TooltipProvider> {/* Add this wrapper */}
+        <App />
+      </TooltipProvider>
     </LanguageProvider>
   </StrictMode>
 );
